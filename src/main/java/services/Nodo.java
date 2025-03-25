32e57fe9a64f6fcf1fc05b1f1f158eca3eff6fad
Pedro-pchic <143863789+Pedro-pchic@users.noreply.github.com>
2025-03-24 22:32:25 -0600

    nodo

---
+++ src/main/java/services/Nodo.java
@@ -0,0 +1,25 @@
+package services;
+
+import lombok.Data;
+
+@Data
+public class Nodo {
+    public Object data;
+    public Nodo sig;
+
+    public Nodo(Object data, Nodo sig) {
+        this.data =data;
+        this.sig = sig;
+    }
+
+    public Nodo(Object data){
+        this(data, null);
+    }
+    @Override
+    public String toString() {
+        return "Nodo{" +
+                "data=" + data +
+                ", sig=" + sig +
+                '}';
+    }
+}
---
+++ src/test/java/services/NodoTest.java
@@ -0,0 +1,22 @@
+package services;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class NodoTest  {
+
+    public Nodo n;
+
+    @Before
+    public void setUp() throws Exception{
+        n = new Nodo(1, null);
+    }
+
+    @Test
+    public void tesToString(){
+        String esperado = "nodo{data=1, sig=null}";
+        String obtenido = n.toString().toLowerCase();
+        assertEquals( esperado, obtenido);
+    }
+}
